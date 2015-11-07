94ff617da4a2ec1da31d81d82333a6fe5815e5ee
Bernd Bestel <bernd@berrnd.de>
2015-11-07 15:51:54 +0100

    Fix missing files in release ZIP didn't work, will do this later...

--- build.bat
+++ build.bat
@@ -8,4 +8,4 @@ for /f "tokens=*" %%a in ('type version.txt') do set version=%%a
 
 del "%releasePath%\shareical_%version%.zip"
 "build_tools\7za.exe" a -tzip -r "%releasePath%\shareical_%version%.zip" "%projectPath%\*.*" -xr!.* -xr!build_tools -xr!build.bat -xr!composer.json -xr!composer.lock -xr!composer.phar -xr!shareical.phpproj -xr!shareical.phpproj.user -xr!shareical.sln
-"build_tools\7za.exe" d -tzip -r "%releasePath%\shareical_%version%.zip" components
+rem "build_tools\7za.exe" d -tzip -r "%releasePath%\shareical_%version%.zip" components
