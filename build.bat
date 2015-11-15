721c969c97eaf232a6e7f5f5518a1a2ae94fb185
Bernd Bestel <bernd@berrnd.de>
2015-11-15 17:42:52 +0100

    Another fix for release ZIP creation

--- build.bat
+++ build.bat
@@ -8,4 +8,4 @@ for /f "tokens=*" %%a in ('type version.txt') do set version=%%a
 
 del "%releasePath%\shareical_%version%.zip"
 "build_tools\7z.exe" a -r "%releasePath%\shareical_%version%.zip" "%projectPath%\*" -xr!.* -xr!build_tools -xr!build.bat -xr!composer.json -xr!composer.lock -xr!composer.phar -xr!shareical.phpproj -xr!shareical.phpproj.user -xr!shareical.sln
-"build_tools\7z.exe" d "%releasePath%\shareical_%version%.zip" components
+"build_tools\7z.exe" d "%releasePath%\shareical_%version%.zip" components config.php
